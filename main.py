0376a817b35ccaf27c388786de1d342e86ec0111
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-10 19:10:08 +0300

    Created a test file for the main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Credential:
     """
 
     def __init__(self,account, username, password):
-        self.account = account
+        self.account_name = account
         self.username = username
         self.password = password
 
@@ -22,5 +22,48 @@ class Credential:
         delete_credential method deletes the credential that the user no longer needs
         """
         Credential.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_account(cls, account):
+        """
+        This method takes in an account and matches an account that matches that account.
+
+        Args:
+            account: Account to search for
+        Returns:
+        The aacount that matches the account searched for.
+        """
+
+        for account in cls.credentials_list:
+            if account.account_name == account:
+                return account
+    
+    @classmethod
+    
+    def account_details_exist(cls, account):
+        """
+        Method that checks if the account exists
+        """
+        for account in cls.credentials_list:
+            if account.account_name == account:
+                return True
+
+        return False
+    
+    @classmethod
+    def authentication(cls, username, password):
+
+        for authentic in cls.credentials_list:
+            if authentic.username == username and authentic.password == password:
+                return authentic 
+
+    
+    @classmethod
+    def display_details(cls)
+        """
+        This method returns the credentials list
+        """
+        return cls.credentials_list
+            
 
     
